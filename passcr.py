b34c79649b975377c591e55e9de53a0f5c3cde4c
Xrios <92352929+Xrios@users.noreply.github.com>
2021-10-12 03:24:51 -0400

    passcr.py
    “For Educational Purposes Only”

---
+++ passcr.py
@@ -0,0 +1,33 @@
+import hashlib
+
+flag = 0
+counter = 0
+
+pass_hash = input("Enter md5 hash: ")
+
+wordlist = input("File name: ")
+
+try:
+    pass_file = open (wordlist, "r")
+except
+    print("No file found")
+    quit()
+
+for word in pass_file:
+
+    enc_wrd = word.encode('utf-8')
+    digest = hashlib.md5(enc_wrd.strip()).hexdigest
+
+    # print(word)
+    # print(digest)
+    # print(pass_hash)
+    counter +=1
+
+    if digest == pass_hash:
+        print("Passowrd found")
+        print("password is " + word)
+        flag = 1
+        break
+
+if flag == 0:
+    print("password/passphrase is not in the list")
